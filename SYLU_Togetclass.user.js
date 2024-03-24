cbc9535dcdcb55042e04b6785c9e969c108c6cee
Endorsie <wyk@endorsie.top>
2024-03-24 13:17:29 +0800

    Add files via upload

---
+++ SYLU_Togetclass.user.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         SYLU_Togetclass
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Hope we can get the classes we desire all.
+// @author       Endorsie
+// @grant        none
+// @match        https://jxw.sylu.edu.cn/xsxk/zzxkyzb_cxZzxkYzbIndex.html?gnmkdm=N253512&layout=default&su=(学号，填写后删除括号)
+// ==/UserScript==
+
+(function() {
+    'use strict';
+     /*弹窗自动确认*/
+    window.confirm = function() { return true; };  
+    window.alert   = function() { return true; };   
+
+    const registrationInterval = 500; // 500毫秒抢一次课；
+    const pageRefreshTimeout = 30000; // 30秒刷新一次页面；
+
+   let registrationAttempt = setInterval(function() {
+        generateMixed('课程代码1', '课程代码2');        // 替换为实际的课程代码
+    }, registrationInterval);
+
+    setTimeout(function() {
+        clearInterval(registrationAttempt); // 30秒后停止尝试
+        window.location.reload(); // 刷新页面
+    }, pageRefreshTimeout);
+})();
